f77946bfa58839251394b4907c05ef1233ec79f2
Ashutosh Sahoo <ashutoshsahoo27@gmail.com>
2023-07-09 13:33:26 +0530

    new commit in dev

--- new.js
+++ new.js
@@ -1,2 +1,5 @@
 console.log('new project without git')
 console.log('new project with git now')
+console.log('new project with git 3')
+console.log('new project with git 4')
+console.log('new project with git 5')
